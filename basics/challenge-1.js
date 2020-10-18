fc9378ca74c5c4d0046badefe9272960312e0f33
JazAduhene <you@example.com>
2020-10-18 21:12:32 +0100

    Concatenation, strings + challenge 1

---
+++ basics/challenge-1.js
@@ -0,0 +1,7 @@
+//Use of concatenation operator
+
+let city = 'London'
+let country = 'England'
+let location = city + ', ' + country 
+
+console.log(location)
---
+++ basics/strings.js
@@ -0,0 +1,11 @@
+//Prints my name and my fullname
+let name = 'Jaz Aduhene'
+let firstName = 'Jaz'
+let lastName = "Aduhene"
+
+//Below used concatenation to combine strings
+let fullName = firstName + ' ' + lastName  
+
+console.log(name)
+console.log(firstName)
+console.log(fullName)
