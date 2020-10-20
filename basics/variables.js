7aaf1069f0751ff8b75bcc989aa11db16f38400f
JazAduhene <you@example.com>
2020-10-20 21:01:59 +0100

    Variables + learned rules about variables

---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+// Rule 1. Below an example of invalid variable, cannot define a variable twice
+
+let petName = 'Jah'
+//let petName = 'Spot'
+
+//Below I have reassigned the variable value
+petName = 'Spot'
+
+console.log(petName)
+
+// Rule 2. There are rules related to variable names
+// Variable name has to start with a letter, $ or _. A number can never be the first character
+let test_ = 2
+let result = 3 - 4
+
+// Rule 3. Variable names cannot be reserved keywords, for example the word let, it had s specific function so cannot be used as a vaiable 
+
