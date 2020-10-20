0bb0be928eafdaa4b2a6b8bbce7b13dd4461ccde
JazAduhene <you@example.com>
2020-10-20 21:22:48 +0100

    Program that converts temperature

--- basics/challenge-3.js
+++ basics/challenge-3.js
@@ -1,7 +1,7 @@
 // Program that converts temperature
 // Order of operation PEMDAS - Parentheses (), Exponents, Multiply *, Divide /, Add +, Subtract -
 
-let fahrenheit = 32
+let fahrenheit = 50
 let celsius = (fahrenheit - 32) * 5 / 9 // Calculates celsius
 let kelvin = (fahrenheit - 32) * 5 / 9 + 273.15 // Calculates Kelvin
 
