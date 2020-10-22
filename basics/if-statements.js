d0d0668c9f978bdc40bf0163fe39ad25a17e8465
JazAduhene <you@example.com>
2020-10-22 07:29:19 +0100

    Used the if statement to print a message with conditions using operators

---
+++ basics/if-statements.js
@@ -0,0 +1,31 @@
+// === -  is the equality operator
+// !== - not equal operator
+// < - less than operator
+// > - greater than operator
+// <= - less than or equal to operator
+// >= - greater than or equal to operator
+
+/*
+let temp = 5
+let isFreezing = temp <= 4 
+
+if (isFreezing) {
+    console.log('It is freezing outside!')
+} else {
+    console.log('It is not cold today!')
+}
+*/
+
+// Program above is correct, the program below is a simplified version.
+
+let temp = 41
+
+if(temp <= 5) {
+    console.log('It is freezing outside!')
+}
+
+if (temp >= 40) {
+    console.log('It is way to hot outside!')
+}
+
+// Used the if statement to print a message with conditions using operators 
