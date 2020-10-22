164775edc49be6ce0ed0ad21c4b9151584dbfca3
JazAduhene <you@example.com>
2020-10-22 07:30:11 +0100

    Program using ifstatements, conditional program to print message about child and senior discount, depending on age

---
+++ basics/challenge-5.js
@@ -0,0 +1,10 @@
+// Program using ifstatements, conditional program to print message about child and senior discount, depending on age.
+
+let age = 65
+if (age <= 7) {
+    console.log('You are eligible for the Child Discount!')
+}
+
+if (age >= 65) {
+    console.log('You are eligible for the Senior Discount!')
+}
