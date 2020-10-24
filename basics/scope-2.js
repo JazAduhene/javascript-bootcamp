6e82d451aeff0e8e5a6cfce58cfc7a4e9768e601
JazAduhene <you@example.com>
2020-10-24 21:38:10 +0100

    More about scopes, variable shadowing

---
+++ basics/scope-2.js
@@ -0,0 +1,21 @@
+// Global ()
+ // Local ()
+   // Local
+ // Local
+
+// let name = 'Jasmine'
+
+if (true) {
+    // let name = 'Jon' // Variable shadowing, the local variable is hiding the global, basically overrides it.
+    
+    if (true) {
+        name = 'Jean' // This line changes name from Jon to Jean
+        console.log(name) // Prints Mike
+    }
+}
+
+if (true) {
+    console.log(name) // Prints Jon because it is outside the scope it falls back on the global not local.
+}
+
+// Leaked global (not good) happens if we assign a value to a variable but that variable wasnt explicitly defined.
