d1d7894b7862e0b3ad6ac841a973b37f535ac866
JazAduhene <you@example.com>
2020-10-24 21:33:54 +0100

    Logical And Operator to print, program that prints nice weather if its between 20 and 30 degrees celsius

---
+++ basics/logical-and-or.js
@@ -0,0 +1,20 @@
+let temp1 = 38
+let temp2 = 49
+
+// Logical And Operator to print, program that prints nice weather if its between 20 and 30 degrees celsius.
+// Anything out of the created range of temperature 20 - 40 will not print a message.
+// Logical And Operator - True if both sides are true, false otherwise.
+
+
+if (temp1 >= 20 && temp1 <= 40) {
+    console.log('It is pretty nice out')
+} 
+
+
+// Logical Or operator - Atleast 1 side has to be true.
+
+if (temp2 <= 0 || temp2 >= 50) {
+    console.log('Do not go outside')
+} else {
+    console.log('Do what you want')
+}
