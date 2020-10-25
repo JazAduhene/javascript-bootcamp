b00123091a821701f318a627de0f904ae9efa301
JazAduhene <you@example.com>
2020-10-25 17:26:48 +0000

    Functions: A function consists of: input (known as an argument), code and output

---
+++ functions/Challenges/challenge-1.js
@@ -0,0 +1,12 @@
+// Function to convert fahrenheit to celsius 
+
+let convertFahrenheitToCelsius = function (fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5 / 9
+    return celsius
+}
+
+let tempOne = convertFahrenheitToCelsius(32)
+let tempTwo = convertFahrenheitToCelsius(68)
+
+console.log(tempOne)
+console.log(tempTwo)
