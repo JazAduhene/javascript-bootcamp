85137c4b4e8005bd057d8f9cbe5add86ae939a65
JazAduhene <jasmine.aduhene@hotmail.com>
2021-02-01 23:50:03 +0000

    Global scope, local scope

---
+++ functions/function-scope.js
@@ -0,0 +1,21 @@
+// Global scope (convertFahrenheitToCelsius, tempOne, tempTwo)
+    // Local scope (fahrenheit - which is the function argument, celsius)
+        // Local scope (isFreezing)
+let convertFahrenheitToCelsius = function (fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5 / 9
+
+    if (celsius <= 31) { // Just because I create something inside a function doesnt mean its accessible outside the function.
+        let isFreezing = true
+        console.log('Its chilly')
+    } else {
+        console.log('Its warm')
+    }
+
+    return celsius // retruns the temeperature in celcius, which is the converted value
+}
+
+let tempOne = convertFahrenheitToCelsius(90)
+let tempTwo = convertFahrenheitToCelsius(50)
+
+console.log(tempOne)
+console.log(tempTwo)
