1aef4e6f7a472f43276ac6577716a5fe99d7d1e4
JazAduhene <jasmine.aduhene@hotmail.com>
2021-02-15 22:25:03 +0000

    Used MDN documentation to use different properties and methods individually and within function

---
+++ objects/string-methods.js
@@ -0,0 +1,36 @@
+let name = 'Jaz Aduhene';
+let firstName = ' Jaz '
+
+// Length property
+console.log(name.length);
+
+// Convert to upper case (method)
+console.log(name.toUpperCase());
+
+// Convert to lower case
+console.log(name.toLowerCase());
+
+// Includes Method
+let password = '123abc$';
+console.log(password.includes('password'));
+
+// Trim Method 
+console.log(firstName.trim());
+
+// Challenge area
+
+let isValidPassword = function (password) {
+    return password.length > 8 && !password.includes('password');
+}
+
+// Can use if statement but to shorten the function and get the same result, I can return 
+//     if (password.length > 8 && !password.includes('password')) {
+//         return true; 
+//     } else {
+//         return false;
+//     }
+// }
+
+console.log(isValidPassword('sfhsjb'));
+console.log(isValidPassword('JazAduhene'));
+console.log(isValidPassword('jaspassword'));
