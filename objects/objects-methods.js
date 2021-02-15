0d985642b374b3b782ddf96b0f3d114034c26188
JazAduhene <jasmine.aduhene@hotmail.com>
2021-02-15 21:25:41 +0000

    Using methods inside of objects to check restaurant availability

--- objects/objects-methods.js
+++ objects/objects-methods.js
@@ -4,13 +4,21 @@
 let restaurant = {
     name: 'Jazzys',
     guestCapacity: 75,
-    guestCount: 40,
+    guestCount: 0,
     checkAvailability: function (partySize) {
         let seatsLeft = this.guestCapacity - this.guestCount;  // (This) refers to the object, (this) is a way to access the object property.
-        return partySize <= seatsLeft;
+            return partySize <= seatsLeft;
+    },
+    seatParty: function (partySize) {
+        this.guestCount = this.guestCount + partySize;
+    },
+    removeParty: function (partySize) {
+        this.guestCount = this.guestCount - partySize;
     }
+
 }
 
-let status = restaurant.checkAvailability(35);
-
-console.log(status);
+restaurant.seatParty(72); // If I seat 72 people I only have 3 seats left (guestCapacity)
+console.log(restaurant.checkAvailability(4)); // When I check the availability I can't seat 4 people because I only have 3 available seats (will print false) 
+restaurant.removeParty(5); // If I remove 5 people I then have 67 seats, leaving 8 seats available. 
+console.log(restaurant.checkAvailability(4)); // (Prints true) because I now have availability for the 4 seats. 
