06b9820c7d2810a3e103884651e4fd7dc85f56b4
JazAduhene <jasmine.aduhene@hotmail.com>
2021-02-18 22:40:22 +0000

    Manipulating arrays with methods

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,15 @@
 const notes = ['Note 1', 'Note 2', 'Note 3'];
 
+// console.log(notes.pop());
+// notes.push('My new note');
+
+// console.log(notes.shift());
+// console.log(notes.unshift('New'));
+
+// notes.splice(1, 1, 'New second item'); // Can use bracket notation or splice
+
+// Brackets notation -  Accessing an item within  a bracket, for example notes[1]
+
+notes[2] = 'This is now the new note 3';
 console.log(notes.length);
-console.log(notes[notes.length - 2]); // Index starts at 0 for arrays
+console.log(notes); // Index starts at 0 for arrays
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,9 +1,16 @@
 const todo = ['Prey', 'Meditate', 'Eat', 'Drink', 'Web Dev']; // Created an array with 5 todos
 
+todo.splice(2, 1);
+todo.push('New item');
+
+console.log(todo.shift());
+
 console.log(`You have ${todo.length} todos!`); // Prints 'You have 5 todos'
-console.log(`Todo: ${todo[0]}, ${todo[todo.length - 2]}`); // Prints the first and second to last items
+console.log(todo);
+
+// console.log(`Todo: ${todo[0]}, ${todo[todo.length - 2]}`); // Prints the first and second to last items
 
 // Another way of doing the above
 
-console.log(`Todo: ${todo[0]}`); // Prints the first item in the array
-console.log(`Todo ${todo[todo.length - 2]}`); // Prints the second to last item in the array
+// console.log(`Todo: ${todo[0]}`); // Prints the first item in the array
+// console.log(`Todo ${todo[todo.length - 2]}`); // Prints the second to last item in the array
