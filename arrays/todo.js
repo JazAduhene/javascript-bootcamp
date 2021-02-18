0aff33b771a5fa91859d2681d4b2ac736096d7a8
JazAduhene <jasmine.aduhene@hotmail.com>
2021-02-18 21:56:50 +0000

    Created 5 todo and printing the first and second to last items

---
+++ arrays/todo.js
@@ -0,0 +1,9 @@
+const todo = ['Prey', 'Meditate', 'Eat', 'Drink', 'Web Dev']; // Created an array with 5 todos
+
+console.log(`You have ${todo.length} todos!`); // Prints 'You have 5 todos'
+console.log(`Todo: ${todo[0]}, ${todo[todo.length - 2]}`); // Prints the first and second to last items
+
+// Another way of doing the above
+
+console.log(`Todo: ${todo[0]}`); // Prints the first item in the array
+console.log(`Todo ${todo[todo.length - 2]}`); // Prints the second to last item in the array
