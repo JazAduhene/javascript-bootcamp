849a23448a6a55469464ce4cd4e4386001b843c5
JazAduhene <jasmine.aduhene@hotmail.com>
2021-03-02 22:56:37 +0000

    Remove an item of todo list by text value

---
+++ arrays/todo-list.js
@@ -0,0 +1,34 @@
+const todoList = [{
+    text: 'Prey',
+    completed: 'true'
+}, {
+    text: 'Meditate',
+    completed: 'true'
+}, {
+    text: 'Watch Movie',
+    completed: 'false'
+}, {
+    text: 'Read Book',
+    completed: 'false'
+}, {
+    text: 'Web Dev',
+    completed: 'true'
+}];
+
+/*
+* NOTE: Convert array to array of objects -> text, completed
+* NOTE: Created function to remove an item of todo list by text value
+*/
+
+const deleteTodo = function (todoList, todoText) {
+    const index = todoList.findIndex(function (todo) {
+        return todo.text.toLowerCase() === todoText.toLowerCase();
+    });
+    // Remove item at index 1
+    if (index > -1) {
+        todoList.splice(index, 1);
+    }
+}
+
+deleteTodo(todoList, 'Watch Movie');
+console.log(todoList);
