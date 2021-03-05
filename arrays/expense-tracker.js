9ceb7fc624e48e72ada587bf300361ff142b64ba
JazAduhene <jasmine.aduhene@hotmail.com>
2021-03-05 20:46:39 +0000

    Added income and account balance to tracker

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -1,6 +1,7 @@
 const account = {
   name: 'Jaz Aduhene',
   expenses: [],
+  income: [],
   // addExpense method, value is equal to a function, takes 2 arguments, description and amount
   addExpense: function (description, amount) {
     // Used to access the array. Use push to add an item to the array
@@ -10,18 +11,35 @@ const account = {
       amount: amount,
     });
   },
-
+  addIncome: function (description, amount) {
+    this.income.push({
+      description: description,
+      amount: amount,
+    });
+  },
   // Added another function that doesn't take any argument, its job is to calculate up all expenses, then print a summary
   getAccountSummary: function () {
     // Total expense that starts of at 0
     let totalExpenses = 0;
-    // Parses in a call back function, which gives access to an individual item
+    let totalIncome = 0;
+    let balance = 0;
+
+    // Parses in a call back function, which gives access to an individual item (expense)
     this.expenses.forEach(function (expense) {
       // Using the amount property to increment the value of 0 to the expense that is parsed in
       totalExpenses = totalExpenses + expense.amount;
     });
+
+    // Parses in a call back function, which gives access to an individual item (expense)
+    this.income.forEach(function (income) {
+      totalIncome = totalIncome + income.amount;
+    });
+
+    // Calculating the balance
+    balance = totalIncome - totalExpenses;
+
     // Template string that prints name assigned to the account and the amount in total expenses in a sentence, depending on value parsed in.
-    return `${this.name} has $${totalExpenses} in expenses`;
+    return `${this.name} has a balance of $${balance}. $${totalIncome} in income. $${totalExpenses} in expenses`;
   },
 };
 
@@ -29,5 +47,7 @@ const account = {
 account.addExpense('Rent', 950);
 // Adds an expense to the account, assigning a description and a value
 account.addExpense('Coffee', 2);
+// Adds income to the account, assigning a description and a value
+account.addIncome('Work', 1000);
 // Prints account summary, the template string adding the rent and coffee expenses together
 console.log(account.getAccountSummary());
